7ac3eef3bed2568ff9b80b3003e0a4c65e9d6157
bahriilhame <bahri.ilhame@gmail.com>
2025-05-20 13:11:52 +0100

    added some func to the description textfield

---
+++ app/backend/db/ApplicationScore.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const matchedApplicationSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "JobApplicantInfo", // ou "User" si tu as une collection séparée
+      required: true,
+    },
+    jobId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "jobs",
+      required: true,
+    },
+    score: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 100,
+      validate: {
+        validator: Number.isFinite,
+        msg: "Score must be a valid number between 0 and 100",
+      },
+    },
+    matchedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { collation: { locale: "en" }, timestamps: true }
+);
+
+module.exports = mongoose.model("MatchedApplication", matchedApplicationSchema);
